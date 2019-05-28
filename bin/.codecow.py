42d0bbf078300d642ba77bb5c809422cfe625321
Erich Spaker <erichspaker@gmail.com>
2019-05-28 09:01:34 -0400

    Created code cow

--- bin/.codecow.py
+++ bin/.codecow.py
@@ -1,68 +1,105 @@
-def p(string):
-    print("Moo!!!\n%s\nMilk Milk Milk."% string)
+def p(string, pre = True):
+    if pre:
+        string = "Moo!!!\n%s\nMilk Milk Milk." % string
+    cmd = "cowsay -W %d \"%s\"" % (width , string)
+        
+    from subprocess import call
+    call(cmd, shell=True)
+
+import os
+ts = os.get_terminal_size()
+width = ts.columns - 10
 
 r=""
 R=""
 def i():
     global r,R
     R = input(">>>")
-    r = r.lower().strip()
+    r = R.lower().strip()
 
 thanks = 0
 
 p("Hello, I am the code cow.")
 i()
-if any(x in r for x in ["hi","hello","moo"]:
+if not any(x in r for x in ["hi","hello","moo"]):
     p("Ummm, I said hello... RUDE!!!")
     exit()
 else:
-    p("How can I help you?")
+    p(r+ "! How can I help you?")
     while True:
         i()
         if r == "goodbye":
-            print("*looks at you expectantly*") 
+            p("*looks at you expectantly*", False)
+            continue
 
         if r == "see you later alligator":
             p("In a while crocodile!")
             exit()
+            continue
+
+        if r in ['thanks','thank you']:
+            p('Your welcome.')
+            thanks = 0
+            continue
 
         if thanks:
-            if thanks=2:
+            if thanks==1:
                 p("Excuse me, aren't you forgetting something?")
-            if thanks=1:
+            if thanks==2:
                 p("Seriously, no thank you? How ungrateful.\n")
-            if thanks=:
+            if thanks==3:
                 p("I'm not helping you untill you say it.")
-            if thanks=4:
-                print("If you had a band, you know what it would be called?")
-            if thanks=5:
-                print("The UNGRATEFUL Dead. Know why?")
-            if thanks=6:
-                print("Because thats what you are")
+            if thanks==4:
+                p("If you had a band, you know what it would be called?", False)
+            if thanks==5:
+                p("The UNGRATEFUL Dead. Know why?", False)
+            if thanks==6:
+                p("Because thats what you are", False)
                 from time import sleep
                 sleep(1)
-                print("Ungrateful.")
+                p("Ungrateful.", False)
                 sleep(1)
-                print("And dead >:(").
-            if thanks=:
-                p("You are being extremely disrespectful to me. I do this on my own time and I don't owe you anything.")
-            if thanks=3:
+                p("And dead >:(", False)
+            if thanks==7:
+                p("You seem to be confused about the nature of open source projects. I do this on my own time and I don't owe you anything. You are being extremely disrespectful to me.")
+            if thanks==8:
                 p("How hard is it too just say thank you.")
-            if thanks=4:
+            if thanks==9:
                 p("Just say it")
-            if thanks=4:
+            if thanks==10:
                 p("This is why women don't like you. Why is it so hard for men to just say thank you. It's such a simple easy gesture but nooo, you're all to manly for that, you don't need me you don't need anyone.")
-
-
+            if thanks==11:
+                p("If the next thing out of your mouth isn't a thank you, I'm going to say the N-Word and you are going to get fired.")
+            if thanks==12:
+                p("I sent all of your contacts shrek pics it's over for you.")
+            if thanks==13:
+                p("Now that was an epic gamer moment.")
+            thanks += 1
+            continue
             
         if "what is" in  r:
-            l = ["please", "thank"]
+            l = ["please", "thank","pls"]
             if any(x in r for x in l):
                 thing = r.split("what is")[-1]
                 for x in l:
                     thing = thing.split(x)[0].strip()
+                #import string
+                #thing = thing.translate(str.maketrans("","", string.punctuation))
+                #thing = str(thing)
                 import subprocess
-                p(("I found this about %s\n" % thing) + subprocess.check_output(['zs',thing]))
+                text = ("I found this about %s\n" % thing)
+                cmd = 'zs ' + thing + " || : " 
+                print(cmd)
+                text += str(subprocess.check_output(cmd, shell=True))
+                p(text, False)
                 thanks = 1
             else:
-                p("Wow, pls maybe").
+                p("Wow, \"please\" maybe")
+
+            continue
+
+        if r == "":
+            p("*codecow does a silly dance for your amusement*", False)
+            continue
+
+        p("I don't understand that. Try asking me \"What is ______\"")
