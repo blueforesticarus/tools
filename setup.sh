287eb8ba3d22376cef831099ae39c13f14298af1
Erich Spaker <erichspaker@gmail.com>
2019-08-29 16:00:01 -0400

    improved setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,42 @@
 D=`dirname "$(readlink -f "$0")"`
+echo "$D"
+PATH=$PATH:$D/bin
+
 chmod +x $D/bin/*
-ln -s $D/bin ~
-ln -s $D/Makefile ~
+
+read -p "Create symlink to tools/bin? (Y/n): " YN
+case ${YN:0:1} in
+    n|N )
+        SYM=$D/bin
+    ;;
+    * )
+        read -e -p "Symlink location: " -i "~/.tools" SYM 
+        SYM=${SYM/#\~/$HOME}
+        ln -s -Tf "$D/bin" $SYM
+    ;;
+esac
+
+read -p "Create symlink to Makefile to home dir? (Y/n): " YN
+case ${YN:0:1} in
+    n|N )
+            
+    ;;
+    * )
+        ln -s $D/Makefile ~
+    ;;
+esac
+
+
+echo "You need to add this line to your .bashrc:"
+echo "PATH=\$PATH:$SYM"
+
+read -p "Add automatically? (Y/n):  " YN
+case ${YN:0:1} in
+    n|N )
+        
+    ;;
+    * )
+        S="PATH=\$PATH:$SYM"
+        grep "$S" ~/.bashrc || echo "$S" >> ~/.bashrc
+    ;;
+esac
