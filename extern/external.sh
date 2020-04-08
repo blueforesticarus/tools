c7c3b9c8d8d3cbfb11330cd3008f0771da90ca06
Erich Spaker <erichspaker@gmail.com>
2020-04-08 11:28:58 -0400

    android menus with rofi

--- extern/external.sh
+++ extern/external.sh
@@ -7,3 +7,16 @@ cd DaveScripts && find * -type f -name "*.sh" -exec ln -snf ../DaveScripts/{} ..
 cd ../bin
 wget https://waf.io/waf-2.0.17 -O waf
 chmod +x * 
+
+ln -sfn ../rofi_android_theme/android android
+chmod +x android
+
+cd ../rofi_android_theme
+mkdir -p ~/.local/share/fonts
+cp feather.ttf ~/.local/share/fonts/
+fc-cache
+
+cd ~/.config/rofi
+ln -sfn "`i root`/extern/rofi_android_theme/" android
+
+
