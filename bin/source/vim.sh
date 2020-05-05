f244ddce7598e755e31fdcb8517158ca90d54fc8
Erich Spaker <erichspaker@gmail.com>
2020-05-05 14:29:01 -0400

    apparently alot of things

---
+++ bin/source/vim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+KITTY_DEFAULT_OPACITY="$( rg 'background_opacity ([0-9\.]*)$' `i root`/config/kitty/kitty.conf -o -r '$1')"
+KITTY_DEFAULT_PADDING="$( rg 'window_padding_width (.*)$' `i root`/config/kitty/kitty.conf -o -r '$1' )"
+
+vim () {
+    no_padding nvim
+}
+
+no_padding () {
+    if [ "$TERM" = "xterm-kitty" ]; then  
+        kitty @set-spacing padding=0 
+        kitty @set-background-opacity 1 
+    fi
+    APP="$1"
+    shift
+    $APP $@
+
+    if [ "$TERM" = "xterm-kitty" ]; then 
+        kitty @set-background-opacity "$KITTY_DEFAULT_OPACITY" 
+        kitty @set-spacing padding=$KITTY_DEFAULT_PADDING
+    fi
+}
