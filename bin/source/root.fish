28d79cd017e7e18c24825ec3c8d82ad771f2a0a4
Erich Spaker <erichspaker@gmail.com>
2020-12-14 10:46:28 -0500

    added "out" function which takes me out of the current filesystem.
    Usefull when I'm in a mounted sdcard image and want to navigate back to the sdcard directory to unmount the card.

--- bin/source/root.fish
+++ bin/source/root.fish
@@ -4,3 +4,16 @@ function root
 end
 complete -c root -e
 complete -c root -f -a "(cd (hg root) && __fish_complete_directories | cut -d \t -f 1)" -d ""
+
+function out
+    set -l D ./
+    while ! mountpoint $D >/dev/null
+        set D $D../
+    end
+    if test (realpath $D) = "/"
+        echo "Current Directory is on root filesystem."
+    else
+        cd $D/../
+        pwd
+    end
+end
